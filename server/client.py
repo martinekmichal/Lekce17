062b7059762c53f07f6e393811fa0632187cffa4
martinekmichal <martinek.m.84@gmail.com>
2024-08-08 20:17:43 +0200

    server komunikace

--- server/client.py
+++ server/client.py
@@ -3,8 +3,15 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 12345))
 
-client_socket.sendall("Ahoj, server!".encode())
-response = client_socket.recv(1024).decode()
-print(f"Odpoveď zo servera: {response}")
+try:
+    while True:
+        message = input("zadej zproavu pro server: ")
+        client_socket.sendall("Ahoj, server!".encode())
+        response = client_socket.recv(1024).decode()
+        print(f"Odpoveď zo servera: {response}")
+        if message == "konec":
+            break
+finally:
+    client_socket.close()
+    print("Client ukončil komunikaci")
 
-client_socket.close()
--- server/server.py
+++ server/server.py
@@ -8,9 +8,15 @@ print("Server čaká na pripojenia...")
 client_socket, client_address = server_socket.accept()
 print(f"Pripojil sa klient: {client_address}")
 
-message = client_socket.recv(1024).decode()
-print(f"Správa od klienta: {message}")
-client_socket.sendall("Správa prijatá".encode())
-
-client_socket.close()
-server_socket.close()
+try:
+    while True:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            break
+        print(f"Správa od klienta: {message}")
+        response = input("Zadejte odpověď: ")
+        client_socket.sendall("Správa prijatá".encode())
+finally:
+    client_socket.close()
+    server_socket.close()
+    print("Server ukončil spojení.")
