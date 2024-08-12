09a2d1356ceda9484b26d514ae52c89d841a646d
martinekmichal <martinek.m.84@gmail.com>
2024-08-12 22:53:05 +0200

    Update Task1_L17_ukol.py
    hotovo

--- UKOL/Task1_L17_ukol.py
+++ UKOL/Task1_L17_ukol.py
@@ -18,5 +18,24 @@ def vypocitej_soucet():
     soucet = sum(seznam_cisel)
     print(f"Součet v seznamu: {soucet}")
 
+def vypocitej_prumer():
+    seznam_naplnen_event.wait()
+    prumer = sum(seznam_cisel) / len(seznam_cisel)
+    print(f"Průměrná hodnota v seznamu je: {prumer}")
 
+def main():
+    vlakno_napln = threading.Thread(target=napln_seznam)
+    vlakno_soucet = threading.Thread(target=vypocitej_soucet)
+    vlakno_prumer = threading.Thread(target=vypocitej_prumer)
+    vlakno_napln.start()
+    time.sleep(1)
+    vlakno_soucet.start()
+    time.sleep(1)
+    vlakno_prumer.start()
 
+    vlakno_napln.join()
+    vlakno_soucet.join()
+    vlakno_prumer.join()
+    print("Hotovo!")
+
+main()
